1935a6ef01aa889bea72bcec3b0c942380a8959a
kumankoishi.py <7iggif@naver.com>
2021-01-11 22:50:14 +0900

    Testing main menu generation

--- js/article.js
+++ js/article.js
@@ -14,15 +14,29 @@ class itemPreviewInfo {
   }
 }
 
-function generateMainMenuList (items) {
+function generateMenuList (items, target) {
   items.forEach(item => {
     fetch('iteminfo/' + item + '.txt').then(function (response) {
       response.text().then(function (text) {
         let info = text.split(',')
         let append = new itemPreviewInfo(info[0], info[1], info[2]).convertToHTML()
         console.log(append)
-        document.querySelector('#main').innerHTML += append
+        document.querySelector(target).innerHTML += append
       })
     })
   })
+}
+
+function generateMainMenuList (items) {
+  generateMenuList(items, '#main')
+}
+
+function pickUpMainMenuItems () {
+  // Originally it's supposed to pick up random 4 items
+  return [1, 2, 3, 4]
+}
+
+function generateMainMenu () {
+  document.querySelector('#content').innerHTML = "<div id=\"main\"></div>" + "<div id=\"showall\">아이템 전체 보기</div>"
+  generateMainMenuList(pickUpMainMenuItems())
 }
